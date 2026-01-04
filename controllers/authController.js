189185d9d81b26c97af1b46e1f74c560b351f02c
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-04 19:10:53 +0100

    add auth controller methods

---
+++ controllers/authController.js
@@ -0,0 +1,30 @@
+const { toLogin } = require("../services/authService");
+const User = require("../models/userModel");
+
+const login = async (req, res) => {
+    try {
+        const credentials = req.body;
+        const { user, token } = await toLogin(credentials);
+        res.status(200).json({ user, token });
+    } catch (error) {
+        res.status(401).json({ mensaje: error.message });
+    }
+};
+
+const me = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({ mensaje: "Usuario no encontrado" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ mensaje: "Error al obtener el usuario" });
+    }
+};
+
+module.exports = {
+    login,
+    me,
+};
