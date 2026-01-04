baad8760e8b85cd75deb839a4d7a58f73bc2eaa0
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-05 00:58:20 +0100

    add vercel serveless support

---
+++ config/db.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+let isConnected = false;
+
+const DB_URL = process.env.DB_URL || "mongodb://localhost:27017/clipperapp";
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(DB_URL);
+    isConnected = db.connections[0].readyState;
+    console.log("nueva conexion a la db");
+  } catch (error) {
+    console.log("error conectando", error);
+  }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DB_URL || "localhost:27017/clipperapp";
+
+const app = express();
 
 app.use(express.json());
+app.use(async (req, res, next) => {
+  await connectDB();
+  next();
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/messages", messageRoutes);
 
-mongoose
-  .connect(DB_URL)
-  .then(() => console.log("conectado a la db"))
-  .catch((error) => console.log("error al conectar a la db:", error));
-
-app.listen(PORT, () => {
-  console.log(`conectado al puerto: ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("CLIPPER API WORKING");
 });
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`conectado al puerto: ${PORT}`);
+  });
+}
+
+module.exports = app;
