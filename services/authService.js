bc10c39fdea138aec6e281a3434894d3d5f7c6fb
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-04 19:10:37 +0100

    add service for auth methods logic

---
+++ services/authService.js
@@ -0,0 +1,19 @@
+const User = require("../models/userModel");
+const { generateToken } = require("../utils/helpers");
+
+const toLogin = async (credentials) => {
+    const { username, password } = credentials;
+    const user = await User.findOne({ username: username });
+
+    if (!user || !(await user.validatePassword(password))) {
+        throw new Error("credenciales invalidas");
+    }
+
+    const token = generateToken({ userId: user._id, rol: user.rol });
+
+    return {user, token};
+};
+
+module.exports = {
+    toLogin,
+};
