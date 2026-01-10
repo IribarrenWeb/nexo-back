7fe34819b37608ed3c490ce93be0351330b8674d
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-10 20:32:39 +0100

    add ValidatorService for field validation with unique and required rules

---
+++ services/validatorService.js
@@ -0,0 +1,62 @@
+const { default: mongoose } = require("mongoose");
+
+class ValidatorService {
+    rules;
+
+    constructor(rules) {
+        this.rules = rules;
+    }
+
+    async validate() {
+        const errors = [];
+
+        const fields = Object.keys(this.rules);
+        for (let field of fields) {
+            const result = await this.validateField(field);
+            if (result) {
+                errors.push(result);
+            }
+        }
+
+        return errors;
+    }
+
+    async validateField(field) {
+        const ruleSet = this.rules[field];
+        if (!ruleSet) return null;
+        const value = ruleSet.value;
+        const rules = ruleSet.rules;
+        let error = '';
+
+        for (let index = 0; index < rules.length; index++) {
+            const rule = rules[index];
+            if (error.length) return; // si ya hay un error, no seguir validando
+
+            const ruleKey = rule.split(':')[0] || null;
+            const ruleParam = rule.split(':')[1] || null;
+            switch (ruleKey) {
+                case 'required':
+                    if (!value || (typeof value === 'string' && !value.trim().length)) {
+                        error = `El campo ${field} es obligatorio.`;
+                    }
+                    break;
+                case 'unique':
+                    const modelKey = ruleParam.split(',')[0];
+                    const fieldKey = ruleParam.split(',')[1] || key;
+                    const exists = await mongoose.models[modelKey].findOne({ [fieldKey]: value });
+                    if (exists?._id) {
+                        error = `El campo ${field} debe ser unico.`;
+                        console.log("unique error", error);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+            
+
+        return error.length ? { field, message: error } : null;
+    }
+}
+
+module.exports = ValidatorService;
