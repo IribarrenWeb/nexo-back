23b17f234654310bc206c4db6c01b1b7614dc144
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-12 01:44:57 +0100

    refactor user routes to ensure all endpoints are protected

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,14 +5,12 @@ const { store, remove, index, update, show, showByUsername, toFollow } = require
 const { protected } = require("../middleware/authMiddleware");
 const adminProtected = require("../middleware/adminMiddleware");
 
-router.use(protected); // todas las rutas estan protegidas
-
-router.get("/", adminProtected, index);
-router.get("/:id", show);
-router.get("/by-username/:username", showByUsername);
+router.get("/", protected, adminProtected, index);
+router.get("/:id", protected, show);
+router.get("/by-username/:username", protected, showByUsername);
 router.post("/", store);
-router.delete("/:id", adminProtected, remove);
-router.put("/:id", update);
-router.put("/follow/:id", toFollow);
+router.delete("/:id", protected, adminProtected, remove);
+router.put("/:id", protected, update);
+router.put("/follow/:id", protected, toFollow);
 
 module.exports = router;
