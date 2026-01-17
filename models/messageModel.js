17c34b1eb5c623d4b199eeccd677d3c697bd7194
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-17 15:44:22 +0100

    refactor message and user models with virtuals and improved notification handling

--- models/messageModel.js
+++ models/messageModel.js
@@ -16,12 +16,21 @@ const messageSchema = new mongoose.Schema(
     content: { type: String, required: true },
     read: { type: Boolean, default: false },
   },
-  { timestamps: true }
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }, 
+    timestamps: true
+  }
 );
 
 messageSchema.post("save", function (message) {
-  // enviar notificacion al frontend
-  toPusher(`messages-${message.to}`, "new-message", message);
+  message.populate("from to", "name lastName username avatar").then(() => {
+    const toUserId = message.to._id.toString();
+    // enviar notificacion al frontend
+    toPusher(`messages-${toUserId}`, "new-message", message);
+  }).catch((err) => {
+    console.error("Error cargando modelos", err);
+  });
 })
 
 module.exports = mongoose.model("Message", messageSchema);
