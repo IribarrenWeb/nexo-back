e91c053cbbca4cbeb8575c9b8fa0f8a752501aba
Keinher Iribarren <iribarrenkeinher@gmail.com>
2026-01-18 11:32:15 +0100

    refactor image decoding utility to validate folder and extension; improve error handling and file path management

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 const Notification = require("../models/notificationModel");
+const path = require('path')
 
 dotenv.config();
 
@@ -56,23 +57,47 @@ const notificate = async (toUserId, title, message, data) => {
 
 /**
  * Funcion para decodificar una imagen en base64 y guardarla en el sistema de archivos
- * - Se inicializa el modulo fs de nodejs
- * - Se elimina el prefijo de la cadena base64
- * - Se crea un buffer a partir de la cadena base64
- * - Se escribe el archivo en el sistema de archivos
- * - Se retorna la ruta completa del archivo guardado
- * @param {*} base64String 
- * @param {*} filePath 
+ * - Valida la carpeta y extension de la imagen
+ * - Decodifica la imagen y la guarda en la carpeta especificada
+ * - Retorna la ruta completa del archivo guardado
+ * 
+ * @param {string} base64String 
+ * @param {string} folder Carpeta donde se guardara la imagen (avatars o banners)
+ * @param {string} fileName Nombre del archivo sin extension
  * @returns Ruta completa del archivo guardado 
  */
-const decodeAndSaveImage = async (base64String, filePath) => {
-    const fs = require('fs').promises;
-    base64String = base64String.split(';base64,').pop();
-    const buffer = Buffer.from(base64String, 'base64');
-    const baseSavePath = '../storage/avatars/' + filePath;
-    await fs.writeFile(baseSavePath, buffer);
-    const fullPath = require('path').resolve(__dirname, baseSavePath);
-    return fullPath;
+const decodeAndSaveImage = async (base64String, folder, fileName) => {
+    try {
+        const valid_folders = ['avatars', 'banners']; // definir carpetas validas
+
+        if (!valid_folders.includes(folder)) throw new Error('La carpeta no es valida');
+
+        const valid_extensions = ['png', 'jpg', 'jpeg']; // definir extensiones validas
+        const extension = base64String.split(';')[0].split('/')[1]; // obtener extension de la imagen
+        
+        // validar extension
+        if (!valid_extensions.includes(extension)) {
+            throw new Error('La extension de la imagen no es valida');
+        }
+        
+        const fs = require('fs').promises; // usar la version de promesas del modulo fs para usar async/await
+        
+        base64String = base64String.split(';base64,').pop(); // eliminar el prefijo de la cadena base64
+        
+        // definimos el nombre del archivo con la extension correcta
+        // para avatares siempre sera png, para banners sera jpg
+        fileName = `${fileName}.${folder == 'avatars' ? `png` : 'jpg'}`;
+
+        const buffer = Buffer.from(base64String, 'base64'); // crear un buffer a partir de la cadena base64
+
+        const filePath = path.join(__dirname, "../storage", folder, fileName); // ruta completa del archivo
+        
+        await fs.writeFile(filePath, buffer); // escribir el archivo
+        
+        return process.env.BASE_API_URL + `/${folder}/${fileName}`; // retornar la ruta completa del archivo guardado
+    } catch (error) {
+        throw new Error('Error al guardar la imagen: ' + error.message);
+    }
 }
 
 module.exports = {
